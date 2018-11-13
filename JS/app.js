f2e0bd6f3b38ac88d207805587e0db783519aca1
chrisshepard <43590660+chrisshepard@users.noreply.github.com>
2018-11-13 16:06:02 -0500

    refactoring

--- JS/app.js
+++ JS/app.js
@@ -5,87 +5,49 @@ const nextLevel = "Next Level";
 const secondsInterval = 1000;
 const dotsPerLevel = 10;
 const numberofLevels = 10;
-
+var executed = false;
 var multiplier = 0;
 var currentDot = 1;
-var timer = document.getElementById("timer");
-var numArea = document.getElementById('numArea');
-var levelViewer = document.getElementById('levelViewer');
-var bubbleSize = document.getElementsByClassName("dot");
+var level = multiplier + 1;
+var timeRemaining = maxTimeLimit;
+var tCounter = document.getElementById("timer");
 var pBar = document.getElementById("pBar");
-
-function time() {
-    var tCounter = timeLimit;
-    tCounter--;
-    timeLimit--;
-    pBar.style.width = ((tCounter * 5) + "%");
-    if ((tCounter * 5 >= 60)) {
-        pBar.className = "progress-bar progress-bar-striped progress-bar-animated bg-success";
-    } else if ((tCounter * 5 > 30 && tCounter * 5 < 60)) {
-        pBar.className = "progress-bar progress-bar-striped progress-bar-animated bg-warning";
-    } else {
-        pBar.className = "progress-bar progress-bar-striped progress-bar-animated bg-danger";
-    }
-
-    if (timeLimit >= 0) {
-        timer.innerHTML = timeLimit;
-    } else {
-        numArea.innerHTML = "Try Again!";
-        pBar.style.width = "100%";
-        clearInterval(timerStart);
-    }
-}
-
-function setTimeLimit() {
-    timer.innerHTML = maxTimeLimit;
-}
+var playArea = document.getElementById("playArea");
+var itemUL = document.getElementById("numArea");
 
 var game = {
-    numberArray: [],
-    ranNums: [],
+    numbers: [],
+    ranNumbers: [],
     startGame: function () {
-        if (playButton.innerHTML === initialState) {
-            this.clearArrays();
-            levelViewer.innerHTML = "";
-            numArea.innerHTML = "";
-            multiplier = 0;
-            currentDot = 1;
-            playButton.innerHTML = restartState;
-            this.viewSquence();
-        } else if (playButton.innerHTML === restartState) {
-            playButton.innerHTML = initialState;
-            levelViewer.innerHTML = "";
-            clearInterval(timerStart);
-            setTimeLimit();
-            pBar.style.width = "100%";
-            pBar.className = "progress-bar progress-bar-striped progress-bar-animated bg-success";
-            numArea.innerHTML = "<p>Click the numbers from lowest to highest. <br> Count to 100 to Complete the Game!</p>";
-        } else if (playButton.innerHTML === nextLevel) {
-            numArea.innerHTML = "";
-            currentDot = 1;
-            multiplier++;
-            playButton.innerHTML = restartState;
-            this.viewSquence();
-        } else {
-            levelViewer.innerHTML = "";
-            playButton.innerHTML = initialState;
-        }
+        this.generateNumbers();
+        this.shuffleNumbers();
+        view.displayGame();
     },
-    viewSquence: function () {
-        view.levelViewer();
-        view.startTimer();
-        view.createDots();
-    },
-    fillDots: function () {
-        for (var i = 0; i < 10; i++) {
+    generateNumbers: function () {
+        for (i = 0; i < dotsPerLevel; i++) {
             var num = (i + 1) + (multiplier * dotsPerLevel);
-            game.numberArray.push(num);
+            this.numbers.push({
+                value: num,
+                verified: undefined,
+            }
+            );
+            if (level <= 2) {
+                levels.randomLocation();
+            } else if (level <= 5) {
 
+            } else if (level <= 7) {
+
+            } else if (level < 10) {
+
+            } else if (level === numberofLevels) {
+
+            };
         }
+
     },
-    shuffleDots: function () {
-        var nums = this.numberArray;
-        ranNums = this.ranNums;
+    shuffleNumbers: function () {
+        var nums = game.numbers;
+        ranNums = game.ranNumbers;
         i = nums.length,
             j = 0;
         while (i--) {
@@ -93,68 +55,161 @@ var game = {
             ranNums.push(nums[j]);
             nums.splice(j, 1);
         }
-    },
-    clearArrays: function () {
-        this.numberArray = [];
-        this.ranNums = [];
-    }
-};
 
-var handler = {
-    startGame: function () {
-        game.startGame();
     },
-}
-
-var view = {
-    createDots: function () {
-        game.fillDots();
-        game.shuffleDots();
-        for (var i = 0; i < 10; i++) {
-            var number = game.ranNums[i];
-            var bubble = document.createElement('span');
-            bubble.className = "dot";
-            bubble.id = "bubble" + number;
-            bubble.innerHTML = number + (multiplier * dotsPerLevel);
-            bubble.onclick = this.verifyNum;
-            numArea.append(bubble);
-        }
-    },
-    startTimer: function () {
-        timer.innerHTML = maxTimeLimit;
-        timeLimit = maxTimeLimit;
-        timerStart = setInterval(time, secondsInterval);
-        timeLimit = maxTimeLimit;
-    },
-    levelViewer: function () {
-        var level = document.createElement("div");
-        levelViewer.innerHTML = " ";
-        level.className = "level";
-        level.innerHTML = "Level " + (multiplier + 1);
-        levelViewer.append(level);
-    },
-    verifyNum: function (element) {
-        if (currentDot + (multiplier * dotsPerLevel) == element.srcElement.innerHTML) {
-            element.srcElement.className = "correct dot";
-            if (currentDot === dotsPerLevel && timer.innerHTML !== "0") {
-                numArea.innerHTML = "Good Job!";
-                timer.innerHTML = maxTimeLimit;
-                pBar.style.width = "100%";
-                pBar.className = "progress-bar progress-bar-striped progress-bar-animated bg-success";
-                clearInterval(timerStart);
-                if (multiplier == (numberofLevels - 1)) {
-                    numArea.innerHTML = "You counted to 100! Great Job.";
-                    levelViewer.innerHTML = "";
-                    playButton.innerHTML = "Play Again";
-                } else {
-                    playButton.innerHTML = nextLevel;
-                }
-            }
+    verifyNumber: function (position, value) {
+        if ((value) == currentDot + (multiplier * dotsPerLevel)) {
+            game.ranNumbers[position].verified = true;
             currentDot++;
         } else {
-            if (element.srcElement.className !== "correct dot") {
-                element.srcElement.className = "wrong dot";
-            }
-        }
+            game.ranNumbers[position].verified = false;
+        };
+    },
+    resetGame: function (content) {
+        game.numbers = [];
+        game.ranNumbers = [];
+        timeRemaining = maxTimeLimit;
+        currentDot = 1;
+        itemUL.innerHTML = content;
+        levels.positionDots = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q"
+            , "r", "s", "t"];
+        levels.usedPositions = [];
     }
 }
+
+var levels = {
+    positionDots: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q"
+        , "r", "s", "t"],
+    usedPositions: [],
+    randomLocation: function () {
+        var position = Math.floor(Math.random() * levels.positionDots.length);
+        var x = levels.positionDots[position];
+        levels.positionDots.splice(position, 1);
+        levels.usedPositions.push(x);
+        game.numbers[i].classCode = x
+    }
+
+}
+
+
+
+
+
+var view = {
+    dotStyles: [],
+    displayGame: function () {
+        view.timer();
+
+
+        view.createDots();
+    },
+    createDots: function () {
+        itemUL.innerHTML = "";
+        var result = game.ranNumbers.find(function (element) {
+            return element.value === parseInt(dotsPerLevel + (multiplier * dotsPerLevel));
+        });
+        if (result.verified === true) {
+            game.resetGame("You win");
+        } else {
+            game.ranNumbers.forEach(function (item, position) {
+                var itemLI = document.createElement("li");
+                itemLI.textContent = item.value;
+                itemLI.className = item.classCode;
+                itemLI.id = position;
+
+                if (item.verified === undefined) {
+                    itemLI.classList.add ("dot"); 
+
+                } else if (item.verified === true) {
+                    itemLI.classList.add("dotClicked", "correct");
+                } else {
+                    itemLI.classList.add("dotClicked", "wrong");
+                };
+                itemUL.appendChild(itemLI);
+
+            });
+        }
+        playArea.appendChild(itemUL);
+    },
+
+    timer: function () {
+        var timer = setInterval(myTimer, secondsInterval);
+        var result = game.ranNumbers.find(function (element) {
+            return element.value === parseInt(dotsPerLevel + (multiplier * dotsPerLevel));
+        });
+
+        function myTimer() {
+            if (result.verified === true && timeRemaining > 0) {
+                clearInterval(timer);
+                pBar.className = "progress-bar progress-bar-striped progress-bar-animated bg-success";
+                pBar.style.width = "100%";
+                tCounter.innerHTML = maxTimeLimit;
+            }
+            else if (parseInt(timeRemaining) > 0 && result.verified !== true) {
+                pBar.style.width = ((timeRemaining * 5) + "%");
+                if ((timeRemaining * 5 >= 60)) {
+                    pBar.className = "progress-bar progress-bar-striped progress-bar-animated bg-success";
+                } else if ((timeRemaining * 5) > 30) {
+                    pBar.className = "progress-bar progress-bar-striped progress-bar-animated bg-warning";
+                } else {
+                    pBar.className = "progress-bar progress-bar-striped progress-bar-animated bg-danger";
+                };
+                tCounter.innerHTML = timeRemaining;
+                timeRemaining--;
+            }
+
+            else {
+                pBar.style.width = "0%";
+                tCounter.innerHTML = "0";
+                clearInterval(timer);
+                game.resetGame("You Lose");
+            };
+        }
+    },
+    setUpEventListeners: function () {
+        var dotUL = document.querySelector('ul');
+
+        dotUL.addEventListener('click', function (event) {
+            var elementClicked = event.target;
+            var value = elementClicked.innerHTML;
+            var position = elementClicked.id;
+            if (event.target.classList.contains("correct") === true) {
+                console.log("already checked this number");
+            } else {
+                game.verifyNumber(position, parseInt(value));
+            };
+            view.createDots();
+        });
+    }
+}
+
+view.setUpEventListeners();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
